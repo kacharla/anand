d8ad77c16d7adde85b71be2a14c42e82c700835b
anandkacharla <anand1112@gmail.com>
2022-10-10 20:52:36 +0530

    first commit on branch1

--- main.c
+++ main.c
@@ -2,4 +2,4 @@ C:\Users\anand\OneDrive\Desktop\Playground
 git@github.com:kacharla/playgroung.git
 eJwtjMuqwjAURf8l4w7S2peZC1YQLhdHTkIexxqa9oQ8biviv98gHe3NgrXeRCgFIdxwgoUwohugTacP4gh1CT10sqlqpUVbtr2kvaa0bvP0pCDO45/R4Hf1vZd43FPjE/mlPpf34bSluFWjfHan7VwNF/Vzw7QervQ3zvc5p74Kjy8H2ZMgPPhMg8IvEHo2C3NJWqP4BK/Cg9AM/ZiPwyIF8AxmYWyxop8eFtcsw+aMh8BFJGxJ1n4+/1P/UCU=
 
-ex1 ,ex2,ex3
+ex1 ,ex2,ex3,ex4
